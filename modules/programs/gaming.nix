bab190a2bb69dc82ddddc4c7e65bb825238ec633
Ra77a3l3-jar <raffaelemeo77@gmail.com>
2025-07-10 13:30:50 +0200

    trying to resolve some problems

--- modules/programs/gaming.nix
+++ modules/programs/gaming.nix
@@ -1,32 +1,21 @@
 { config, pkgs, lib, ... }:
 
 {
-  # Gamemode for performance tweaks
-  programs.gamemode.enable = true;
-
-  # Steam and Proton setup
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true;
-    dedicatedServer.openFirewall = false;
-    extraPackages = with pkgs; [
-      mangohud        # In-game overlay and performance metrics
-      protonup-qt     # GUI to manage Proton-GE
-    ];
-  };
-
-  # Lutris for Wine games
-  programs.lutris.enable = true;
-
-  # Modrinth Launcher (PrismLauncher recommended for Modrinth)
   environment.systemPackages = with pkgs; [
-    prismlauncher
+    steam
+    lutris
     gamemode
     mangohud
-    lutris
-    steam
     protonup-qt
-    bottles              # Optional: GUI to manage Wine/Proton environments
+    prismlauncher
+    bottles
   ];
 
+  # For gamemode service if you want to enable it system-wide
+  services.gamemode.enable = true;
+
+  # If you want to add some extra config for steam or proton,
+  # usually you configure inside the apps, not here.
+
 }
+
