4311b4c1d798c3abadfd4d0526e078833506ee3c
Ra77a3l3-jar <raffaelemeo77@gmail.com>
2025-07-10 12:34:20 +0200

    i added the drivers only for the nvidia rtx gpu

--- modules/core/drivers.nix
+++ modules/core/drivers.nix
@@ -1,1 +1,17 @@
+{ config, pkgs, ... }:
+
+{
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.graphics.enable = true;
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    open = false;  # use closed-source kernel module
+  };
+
+  environment.systemPackages = with pkgs; [
+    vulkan-loader
+  ];
+}
 
