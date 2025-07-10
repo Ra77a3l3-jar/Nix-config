18f34193390bf3fd436448dfaea8bbc4042f677c
Ra77a3l3-jar <raffaelemeo77@gmail.com>
2025-07-10 13:59:38 +0200

    faltpak adds the remote only for the user

--- modules/programs/flatpak.nix
+++ modules/programs/flatpak.nix
@@ -17,12 +17,15 @@ in {
   home.packages = [ pkgs.flatpak ];
 
   home.activation.flatpakSetup = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    ${pkgs.flatpak}/bin/flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    # Add Flathub remote for user if not exists
+    ${pkgs.flatpak}/bin/flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo --user
 
+    # Install all flatpak apps for user if not installed
     for app in ${lib.concatStringsSep " " flatpakApps}; do
-      if ! ${pkgs.flatpak}/bin/flatpak info "$app" >/dev/null 2>&1; then
-        ${pkgs.flatpak}/bin/flatpak install -y flathub "$app"
+      if ! ${pkgs.flatpak}/bin/flatpak info --user "$app" >/dev/null 2>&1; then
+        ${pkgs.flatpak}/bin/flatpak install -y --user flathub "$app"
       fi
     done
   '';
 }
+
