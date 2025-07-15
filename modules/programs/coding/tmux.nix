a446d1abffb11c79e524e68c7f2b47f1a33617e2
Ra77a3l3-jar <raffaelemeo77@gmail.com>
2025-07-15 14:59:14 +0200

    boh

--- modules/programs/coding/tmux.nix
+++ modules/programs/coding/tmux.nix
@@ -1,33 +1,40 @@
-programs.tmux = {
-  enable = true;
-  shortcut = "a";
-  baseIndex = 1;
-  newSession = true;
-  escapeTime = 0;
-  secureSocket = false;
+{ config, pkgs, ... }:
 
-  plugins = with pkgs.tmuxPlugins; [
-    sensible
-    tpm
-    better-mouse-mode
-  ];
+{
+  programs.tmux = {
+    enable = true;
+    shortcut = "a";
+    baseIndex = 1;
+    newSession = true;
+    escapeTime = 0;
+    secureSocket = false;
 
-  extraConfig = ''
-    set -g default-terminal "xterm-256color"
-    set -ga terminal-overrides ",*256col*:Tc"
-    set -ga terminal-overrides '*:Ss=\E[%p1%d q:Se=\E[ q'
-    set-environment -g COLORTERM "truecolor"
+    plugins = with pkgs.tmuxPlugins; [
+      sensible
+      tpm
+      better-mouse-mode
+    ];
 
-    set-option -g mouse on
-    bind | split-window -h -c "#{pane_current_path}"
-    bind - split-window -v -c "#{pane_current_path}"
-    bind c new-window -c "#{pane_current_path}"
+    extraConfig = ''
+      set -g default-terminal "xterm-256color"
+      set -ga terminal-overrides ",*256col*:Tc"
+      set -ga terminal-overrides '*:Ss=\E[%p1%d q:Se=\E[ q'
+      set-environment -g COLORTERM "truecolor"
 
-    set -g @plugin 'tmux-plugins/tpm'
-    set -g @plugin 'tmux-plugins/tmux-sensible'
-    set -g @plugin 'yukidoke/tmux-which-key'
+      set-option -g mouse on
+      bind | split-window -h -c "#{pane_current_path}"
+      bind - split-window -v -c "#{pane_current_path}"
+      bind c new-window -c "#{pane_current_path}"
 
-    run '~/.tmux/plugins/tpm/tpm'
-  '';
-};
+      set -g @plugin 'tmux-plugins/tpm'
+      set -g @plugin 'tmux-plugins/tmux-sensible'
+      set -g @plugin 'yukidoke/tmux-which-key'
 
+      run '~/.tmux/plugins/tpm/tpm'
+    '';
+  };
+
+  programs.tmate = {
+    enable = true;
+  };
+}
