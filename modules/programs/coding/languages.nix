ddb8820d190e7d6f51173142db11a0c87314e58b
Ra77a3l3-jar <raffaelemeo77@gmail.com>
2025-07-30 20:31:36 +0200

    deleted ncurses

--- modules/programs/coding/languages.nix
+++ modules/programs/coding/languages.nix
@@ -9,11 +9,6 @@
     gdb
     cmake
 
-    # Utils
-    ncurses.dev
-    ncurses
-    pkg-config
-
     # Java
     jdk
     gradle
@@ -28,9 +23,6 @@
 
   home.sessionVariables = {
     # You can add other environment variables here if needed
-    PKG_CONFIG_PATH = "${pkgs.ncurses.dev}/lib/pkgconfig:$PKG_CONFIG_PATH";
-    C_INCLUDE_PATH = "${pkgs.ncurses.dev}/include:$C_INCLUDE_PATH";
-    LIBRARY_PATH = "${pkgs.ncurses}/lib:$LIBRARY_PATH";
   };
 }
 
