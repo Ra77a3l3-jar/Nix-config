aa65afad5e0054ac6c69e128897617fc5d1f6d98
Ra77a3l3-jar <raffaelemeo77@gmail.com>
2025-08-02 18:53:53 +0200

    possible kitty conf

--- modules/programs/coding/terminals/kitty.nix
+++ modules/programs/coding/terminals/kitty.nix
@@ -1,1 +1,27 @@
+{ config, pkgs, ... }:
+
+{
+  programs.kitty.enable = true;
+
+  programs.kitty.font = {
+    name = "Ubuntu Nerd Font Mono"; # or substitute with fira-code-nerd-font
+    size = 14;
+  };
+
+  programs.kitty.window = {
+    decorations = false;
+    padding = { x = 10; y = 10; };
+    opacity = 0.96;
+  };
+
+  programs.kitty.cursor = {
+    style = "block";
+    blink = true;
+    blink_interval = 0.6;
+  };
+
+  programs.kitty.extraConfig = ''
+    # Put your VibrantInk config here if you want inline
+  '';
+}
 
