281e754f046ecbb7a4efe30a45f5f3ffd4d079f5
Ra77a3l3-jar <raffaelemeo77@gmail.com>
2025-08-06 16:50:40 +0200

    had problems

--- modules/programs/coding/editor/helix/languages.nix
+++ modules/programs/coding/editor/helix/languages.nix
@@ -3,195 +3,187 @@
 {
   programs.helix.languages = {
     language = [
-      # C
       {
         name = "c";
+        scope = "source.c";
         language-servers = [ "clangd" ];
         indent = {
           tab-width = 4;
           unit = "    ";
         };
       }
-      # C++
       {
         name = "cpp";
+        scope = "source.cpp";
         language-servers = [ "clangd" ];
         indent = {
           tab-width = 4;
           unit = "    ";
         };
       }
-      # Java
       {
         name = "java";
+        scope = "source.java";
         language-servers = [ "jdtls" ];
         indent = {
           tab-width = 4;
           unit = "    ";
         };
       }
-      # Zig
       {
         name = "zig";
+        scope = "source.zig";
         language-servers = [ "zls" ];
         indent = {
           tab-width = 4;
           unit = "    ";
         };
       }
-      # Python
       {
         name = "python";
+        scope = "source.python";
         language-servers = [ "pylsp" ];
         indent = {
           tab-width = 4;
           unit = "    ";
         };
       }
-      # Rust
       {
         name = "rust";
+        scope = "source.rust";
         language-servers = [ "rust-analyzer" ];
         indent = {
           tab-width = 4;
           unit = "    ";
         };
       }
-      # Go
       {
         name = "go";
+        scope = "source.go";
         language-servers = [ "gopls" ];
         indent = {
           tab-width = 4;
           unit = "    ";
         };
       }
-      # Markdown
       {
         name = "markdown";
+        scope = "text.markdown";
         language-servers = [ "marksman" ];
         indent = {
           tab-width = 4;
           unit = "    ";
         };
       }
-      # Nix
       {
         name = "nix";
+        scope = "source.nix";
         language-servers = [ "nil" ];
         indent = {
           tab-width = 4;
           unit = "    ";
         };
       }
-      # YAML
       {
         name = "yaml";
+        scope = "source.yaml";
         language-servers = [ "yaml-language-server" ];
         indent = {
           tab-width = 4;
           unit = "    ";
         };
       }
-      # TOML
       {
         name = "toml";
+        scope = "source.toml";
         language-servers = [ "taplo" ];
         indent = {
           tab-width = 4;
           unit = "    ";
         };
       }
-      # JSON
       {
         name = "json";
+        scope = "source.json";
         language-servers = [ "vscode-json-language-server" ];
         indent = {
           tab-width = 4;
           unit = "    ";
         };
       }
-      # XML
       {
         name = "xml";
+        scope = "text.xml";
         indent = {
           tab-width = 4;
           unit = "    ";
         };
       }
-      # JavaScript
       {
         name = "javascript";
-        language-servers = [ "vscode-html-language-server" "vscode-css-language-server" "vscode-eslint-language-server" ];
-        indent = {
-          tab-width = 4;
-          unit = "    ";
-        };
-      }
-      # TypeScript
-      {
-        name = "typescript";
+        scope = "source.js";
         language-servers = [ "typescript-language-server" ];
         indent = {
           tab-width = 4;
           unit = "    ";
         };
       }
-      # PHP
+      {
+        name = "typescript";
+        scope = "source.ts";
+        language-servers = [ "typescript-language-server" ];
+        indent = {
+          tab-width = 4;
+          unit = "    ";
+        };
+      }
       {
         name = "php";
+        scope = "source.php";
         language-servers = [ "phpactor" ];
         indent = {
           tab-width = 4;
           unit = "    ";
         };
       }
-      # Vue
       {
         name = "vue";
+        scope = "source.vue";
         language-servers = [ "vls" ];
         indent = {
           tab-width = 4;
           unit = "    ";
         };
       }
-      # CSS
       {
         name = "css";
+        scope = "source.css";
         language-servers = [ "vscode-css-language-server" ];
         indent = {
           tab-width = 4;
           unit = "    ";
         };
       }
-      # HTML
       {
         name = "html";
+        scope = "text.html";
         language-servers = [ "vscode-html-language-server" ];
         indent = {
           tab-width = 4;
           unit = "    ";
         };
       }
-      # Tailwind (as CSS, but you can add a comment)
-      {
-        name = "tailwindcss";
-        indent = {
-          tab-width = 4;
-          unit = "    ";
-        };
-      }
-      # SQL (for sqlite)
       {
         name = "sql";
+        scope = "source.sql";
         indent = {
           tab-width = 4;
           unit = "    ";
         };
       }
-      # Shell (for httpie, curl, etc.)
       {
-        name = "sh";
+        name = "bash";
+        scope = "source.bash";
         indent = {
           tab-width = 4;
           unit = "    ";
