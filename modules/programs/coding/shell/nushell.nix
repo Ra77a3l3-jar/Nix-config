8530d8cd6bcd9e45356d796c8b65906043e8e272
Ra77a3l3-jar <raffaelemeo77@gmail.com>
2025-08-06 18:36:52 +0200

    maybe not it will work

--- modules/programs/coding/shell/nushell.nix
+++ modules/programs/coding/shell/nushell.nix
@@ -1,13 +1,7 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
-  # Install nushell system-wide
   environment.systemPackages = with pkgs; [ nushell ];
 
-  # Deploy global Nushell environment config
-  environment.etc."nushell/env.nu".text = ''
-    let-env PATH = ($env.PATH | prepend "~/.local/bin" | prepend "~/.nix-profile/bin")
-    let-env MANPAGER = "batman"
-    let-env EDITOR = "hx"
-    let-env VISUAL = "hx"
-  '';
+  environment.pathsToLink = [ "/share/nushell" ];
+}
