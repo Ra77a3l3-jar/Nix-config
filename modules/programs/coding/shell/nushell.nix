db5f40407f93d1c7a7acc9160562fd84336dc60e
Ra77a3l3-jar <raffaelemeo77@gmail.com>
2025-08-06 18:53:27 +0200

    .

--- modules/programs/coding/shell/nushell.nix
+++ modules/programs/coding/shell/nushell.nix
@@ -1,7 +1,25 @@
 { pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [ nushell ];
+  environment.systemPackages = with pkgs; [
+    nushell
+  ];
 
   environment.pathsToLink = [ "/share/nushell" ];
+
+  programs.nushell = {
+    enable = true;
+
+    shellAliases = {
+      ls = "eza";
+      ll = "eza -l";
+      lg = "eza --icons --git -l";
+      lt = "eza --icons --gt -l --tree";
+    };
+
+    environmentVariables = {
+      PATH = "$HOME/.local/bin:$HOME/.nix-profile/bin:$PATH";
+    };
+  };
 }
+
