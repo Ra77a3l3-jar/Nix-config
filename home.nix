c72331dbb02cc5ad69ecf7569fa8a46aec00ca91
Ra77a3l3-jar <raffaelemeo77@gmail.com>
2025-08-06 18:33:07 +0200

    nushell in config

--- home.nix
+++ home.nix
@@ -15,8 +15,6 @@
     ./modules/programs/coding/editor/jetBrains.nix
 
     ./modules/programs/coding/languages/default.nix
-
-    ./modules/programs/coding/shell/nushell.nix
   ];
   
   home.username = "raffaele"; # Replace 'root' with your actual username
--- modules/programs/coding/shell/nushell.nix
+++ modules/programs/coding/shell/nushell.nix
@@ -1,43 +1,13 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
-  programs.nushell = {
-    enable = true;
-    package = pkgs.nushell;
+  # Install nushell system-wide
+  environment.systemPackages = with pkgs; [ nushell ];
 
-    environmentVariables = {
-      PATH = "$HOME/.local/bin:$HOME/.nix-profile/bin:$PATH";
-      MANPAGER = "batman";
-      EDITOR = "hx";
-      VISUAL = "hx";
-    };
-
-    configFile.text = ''
-      # General Aliases
-      alias hm-reload = source ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-      alias nos-rebuild = nixos-rebuild switch --flake ./#nixos --verbose
-      alias ex = exit
-      alias gs = git status
-      alias tx = tmux
-      alias cl = clear
-      alias hs = history
-
-      # Eza Aliases
-      alias la = eza -la --icons
-      alias ltree = eza --tree --level=3 --long --git --icons
-      alias l = eza -l --icons --only-files
-      alias ld = eza -l --icons --only-dirs
-      alias lsd = eza --icons --only-dirs
-      alias ls = eza -l --icons --sort=size
-      alias ltime = eza -l --icons --sort=modified
-      alias lnew = eza -l --icons --sort=modified --reverse
-      alias lperm = eza -l --icons --group-directories-first
-      alias lbig = eza -l --icons --sort=size | take 10
-      alias lg = eza -l --git --icons
-      alias lt = eza --git --tree -l --icons
-
-      # zoxide init for nushell
-      use ~/.zoxide.nu
-    '';
-  };
-}
+  # Deploy global Nushell environment config
+  environment.etc."nushell/env.nu".text = ''
+    let-env PATH = ($env.PATH | prepend "~/.local/bin" | prepend "~/.nix-profile/bin")
+    let-env MANPAGER = "batman"
+    let-env EDITOR = "hx"
+    let-env VISUAL = "hx"
+  '';
