7b2d30afcee07f9d920be1b546162c6e12b89494
Ra77a3l3-jar <raffaelemeo77@gmail.com>
2025-08-06 18:59:05 +0200

    new aliases and zoxide hook

--- modules/programs/coding/shell/nushell.nix
+++ modules/programs/coding/shell/nushell.nix
@@ -5,11 +5,23 @@
     enable = true;
 
     shellAliases = {
-      ls = "eza";
-      ll = "eza -l";
-      la = "eza -la";
-      lt = "eza -T";
+      ls = "eza --icons";
+      l = "eza -l";
+      lg = "eza --icons --git -l";
+      lt = "eza --tree --icons --git -l";
+
+      cl = "clear";
+      ex = "exit";
+      tx = "tmux";
+      
+      
     };
+
+     # Zoxide init hook for Nushell
+    extraConfig = ''
+      zoxide init nushell | save --raw ~/.config/nushell/zoxide.nu
+      source ~/.config/nushell/zoxide.nu
+    '';
 
     environmentVariables = {
       PATH = "$HOME/.local/bin:$HOME/.nix-profile/bin:$PATH";
