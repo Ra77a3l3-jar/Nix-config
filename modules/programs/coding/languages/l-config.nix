635abe361f3c2fef794d0c17f6e11810145d922e
Ra77a3l3-jar <raffaelemeo77@gmail.com>
2025-08-07 10:21:59 +0200

    added a config language .nix

--- modules/programs/coding/languages/l-config.nix
+++ modules/programs/coding/languages/l-config.nix
@@ -2,20 +2,11 @@
 
 {
   home.packages = with pkgs; [
-    # Nix & LSP
-    nixpkgs-fmt
     nil           # LSP per Nix
-    statix        # Linter Nix
 
-    # Markdown
     marksman      # LSP per Markdown
 
-    # YAML / TOML / JSON
     nodePackages.yaml-language-server
     taplo         # TOML LSP
-    
-    # Grammar / Spellcheck
-    ltex-ls
   ];
 }
-
