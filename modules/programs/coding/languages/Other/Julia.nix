20d5c1a34f7619dad703ca6e7d7e4989b69fb047
Ra77a3l3-jar <raffaelemeo77@gmail.com>
2025-08-07 15:37:28 +0200

    added a julia.nix

--- modules/programs/coding/languages/Other/Julia.nix
+++ modules/programs/coding/languages/Other/Julia.nix
@@ -1,1 +1,21 @@
+{ pkgs, ... }:
+{
+
+  programs.helix.languages.language = [
+    {
+      name = "julia";
+      scope = "source.julia";
+      language-servers = [ "julials" ];
+      indent = {
+        tab-width = 4;
+        unit = "    ";
+      };
+    }
+  ];
+
+  home.packages = with pkgs; [
+    julia
+    julia-language-server
+  ];
+}
 
