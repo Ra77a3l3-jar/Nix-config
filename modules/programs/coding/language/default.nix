3715032bb9e6d95708bc0f52f4706984bfac57f6
Ra77a3l3-jar <raffaelemeo77@gmail.com>
2025-08-22 20:47:50 +0200

    Update: new default for language

---
+++ modules/programs/coding/language/default.nix
@@ -0,0 +1,13 @@
+{ ... }:
+
+{
+
+  imports = [
+    #./Systems/default.nix
+    #./Scripting/default.nix
+    #./Full-Stack/default.nix
+    ./Other/default.nix
+    #./Utils/default.nix
+  ];
+  
+}
