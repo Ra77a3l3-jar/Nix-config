03b36307d6e2d1053778141f8954bc85cf047800
Ra77a3l3-jar <raffaelemeo77@gmail.com>
2025-08-23 10:24:08 +0200

    Update: Deleted all of the old language modules i've already changed

--- modules/programs/coding/languages/Other/default.nix
+++
@@ -1,8 +0,0 @@
-{ ... }:
-
-{
-  imports = [
-    ./Julia.nix
-    ./lua.nix
-  ];
-}
--- modules/programs/coding/languages/default.nix
+++ modules/programs/coding/languages/default.nix
@@ -1,12 +1,7 @@
 { ... }:
 {
   imports = [
-    #./java.nix
-    #./rust.nix
-    #./zig.nix
-    #./python.nix
     ./Utils/default.nix
-    #./Other/default.nix
     ./Full-Stack/default.nix
   ];
   
