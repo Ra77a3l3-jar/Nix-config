1ac1891bbd99abbbd7135e809b15cbfe4c3f3f60
Ra77a3l3-jar <raffaelemeo77@gmail.com>
2025-08-23 17:45:02 +0200

    Update: deimporting old languages

--- home.nix
+++ home.nix
@@ -9,7 +9,6 @@
     ./modules/programs/flatpak.nix
     
     ./modules/programs/coding/editor/helix/default.nix
-    ./modules/programs/coding/languages/default.nix
 
     ./modules/programs/coding/language/default.nix
   ];
