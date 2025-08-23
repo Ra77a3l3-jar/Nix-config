b6accf816f4ec09d0207854e1adf9b472a69c866
Ra77a3l3-jar <raffaelemeo77@gmail.com>
2025-08-23 10:11:23 +0200

    New: New Java module compleate

--- modules/programs/coding/language/Other/java.nix
+++ modules/programs/coding/language/Other/java.nix
@@ -1,1 +1,26 @@
+{ pkgs, pkgs-unstable, ... }:
 
+{
+
+  home.packages = with pkgs; [
+    jdk
+    jdt-language-server
+    gradle
+    maven
+  ];
+
+  programs.helix.languages = {
+
+    language = [
+      {
+        name = "java";
+        scope = "source.java";
+        language-servers = [ "jdtls" ];
+        indent = {
+        tab-width = 4;
+        unit = "    ";
+        };
+      }
+    ];
+  };
+}
