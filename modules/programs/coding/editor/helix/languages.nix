975e555fd34f23b1533e8f2831e741eeac856b2c
Ra77a3l3-jar <raffaelemeo77@gmail.com>
2025-08-23 09:49:09 +0200

    Update: deleted c and cpp from languages.nix for helix

--- modules/programs/coding/editor/helix/languages.nix
+++ modules/programs/coding/editor/helix/languages.nix
@@ -6,24 +6,6 @@
     
     language = [
       {
-        name = "c";
-        scope = "source.c";
-        language-servers = [ "clangd" ];
-        indent = {
-          tab-width = 4;
-          unit = "    ";
-        };
-      }
-      {
-        name = "cpp";
-        scope = "source.cpp";
-        language-servers = [ "clangd" ];
-        indent = {
-          tab-width = 4;
-          unit = "    ";
-        };
-      }
-      {
         name = "java";
         scope = "source.java";
         language-servers = [ "jdtls" ];
