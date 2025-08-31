cfff2baf2ef1b4e0adb5ba7c00060e5f84db422c
Ra77a3l3-jar <raffaelemeo77@gmail.com>
2025-08-31 11:17:18 +0200

    New: A new Helix dir with its new config

---
+++ modules/programs/coding/editor/helix/helix.nix
@@ -0,0 +1,48 @@
+{ pkgs, pkgs-unstable, ... }:
+
+{
+  programs.helix = {
+    enable = true;
+    package = pkgs-unstable.helix;
+
+    settings = {
+      #theme = "github_dark_high_contrast";
+      #theme = "ashen";
+      theme = "ayu_evolve";
+      
+      editor = {
+        scrolloff = 999;
+        line-number = "relative";
+        bufferline = "multiple";
+        mouse = false;
+        auto-format = true;
+        true-color = true;
+        auto-completion = true;
+
+        cursor-shape = {
+          insert = "block";
+          normal = "block";
+          select = "block";
+        };
+
+        indent-guides = {
+          render = true;
+          character = "|";
+          skip-levels = 1;
+        };
+
+        statusline = {
+          left = [ "mode" "spinner" "file-name" ];
+          center = [ "version-control" ];
+          right = [ "diagnostics" "selections" "position" "file-encoding" "file-line-ending" "file-type" ];
+          separator = "│";
+          mode = {
+            normal = "NOR";
+            insert = "INS";
+            select = "SEL";
+          };
+        };
+      };
+    };
+  };
+}
