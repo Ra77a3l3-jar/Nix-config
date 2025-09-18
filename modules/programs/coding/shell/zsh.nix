e9eb64322348d861492ae02c7d7a26bfd2202b7e
Ra77a3l3-jar <raffaelemeo77@gmail.com>
2025-09-18 18:00:24 +0200

    feat: updated in zsh.nix

--- modules/programs/coding/shell/zsh.nix
+++ modules/programs/coding/shell/zsh.nix
@@ -12,19 +12,14 @@
     };
     
     interactiveShellInit = ''
-      export PATH="$HOME/.local/bin:$PATH"
       export PATH="$HOME/.nix-profile/bin:$PATH"
       export EDITOR="hx"
       export VISUAL="hx"
-      
-      export GOOGLE_CLOUD_PROJECT="peak-hangout-469915-d6"
-      
+            
       eval "$(zoxide init zsh)"
 
-      alias hm-reload='source ~/.nix-profile/etc/profile.d/hm-session-vars.sh'
       alias nos-rebuild='nixos-rebuild switch --flake ./#nixos --verbose'
       alias nos-test='sudo nixos-rebuild test --flake /etc/nixos#nixos'
-
       alias nos-clean='sudo nix-collect-garbage -d'
     
       alias ex='exit'
