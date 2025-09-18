68f1e52da6019ed1ef9d9db96748e380555fd0bb
Ra77a3l3-jar <raffaelemeo77@gmail.com>
2025-09-18 13:44:35 +0200

    feat: importing networking

--- home.nix
+++ home.nix
@@ -9,6 +9,7 @@
     
     ./modules/programs/coding/editor/default.nix
     ./modules/programs/coding/language/default.nix
+    ./modules/programs/networking/default.nix
   ];
   
   home.username = "raffaele";
