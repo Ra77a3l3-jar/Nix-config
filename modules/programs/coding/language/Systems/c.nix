0935380c42976de20a2d901240b9ea4194d454a7
Ra77a3l3-jar <raffaelemeo77@gmail.com>
2025-10-17 15:52:55 +0200

    feat: new pkgs and libraries for C
    - graphic libraries like GLEW LibGL ecc

--- modules/programs/coding/language/Systems/c.nix
+++ modules/programs/coding/language/Systems/c.nix
@@ -2,27 +2,54 @@
 
 {
   home.packages = with pkgs; [
+    # C toolchain
     clang
     clang-tools
+    lldb
     gdb
+    
+    # Build tools specific to C
     cmake
     cmake-language-server
     gnumake
+    # autoconf
+    # automake
+    # libtool
+    
+    # Graphics Libraries
+    libGL
+    libGLU
+    mesa
+    glfw
+    glew
+    glm
+    SDL2
+    freeglut
+    
+    # X11 libraries
+    # xorg.libX11
+    # xorg.libXi
+    # xorg.libXrandr
+    # xorg.libXcursor
+    # xorg.libXinerama
+    # xorg.libXxf86vm
+    # xorg.libXxf86dga
+    # xorg.libXmu
+    # xorg.libXpm
+    # xorg.libXaw
   ];
 
   programs.helix.languages = {
-
     language = [
       {
         name = "c";
         scope = "source.c";
         language-servers = [ "clangd" ];
         indent = {
-        tab-width = 4;
-        unit = "    ";
+          tab-width = 4;
+          unit = "    ";
         };
       }
     ];
   };
-  
 }
